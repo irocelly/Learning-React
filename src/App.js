b4c665686e96387d81beea8d5d5e025fbe286db1
irocelly <irocelly@gmail.com>
2023-08-20 00:21:30 +0700

    add header navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import Layout, { Content } from "antd/es/layout/layout";
+import Navbar from "./components/layouts/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World ! Our first project using Ant design</h1>
-    </div>
+    <Layout className="layout">
+      <Navbar />
+      <Content>
+        <h1>Hello World ! Our first project using Ant design</h1>
+      </Content>
+    </Layout>
   );
 }
 
